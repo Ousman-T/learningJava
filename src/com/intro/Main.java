b9889eb28ab711a0df5f610d5c2e72a71b52c810
Ousman Touray <otouray14@gmail.com>
2023-09-01 12:14:46 -0400

    Basic Boilerplate

---
+++ src/com/intro/Main.java
@@ -0,0 +1,9 @@
+package com.intro;
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 23;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
